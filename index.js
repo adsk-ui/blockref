0f23057a8154f35e0d7732d1fd5a554c92817f68
admangum <adam.mangum@autodesk.com>
2015-02-24 15:15:19 -0800

    Adds minimatch as dep; adds include/exclude options

--- index.js
+++ index.js
@@ -2,24 +2,46 @@ var through = require('through2');
 var useref = require('node-useref');
 var util = require('gulp-util');
 var fs = require('fs');
-module.exports = function(type, name){
-	return through.obj(function(file, env, cb){
-		var output = useref(file.contents.toString());
+var minimatch = require('minimatch');
+module.exports = function(type, name, pattern){
+        return through.obj(function(file, env, cb){
+                var output = useref(file.contents.toString());
         var assets = output[1];
         var filepaths;
+        var include;
         if( type && assets[type] && name && assets[type][name] ){
-        	filepaths = assets[type][name].assets;
-        	filepaths.forEach(function(path){
-        		this.push(new util.File({
-	    			cwd: file.cwd,
-	    			base: file.base,
-	    			path: file.base + path,
-	    			contents: new Buffer(fs.readFileSync( file.base + path ))
-	    		}));
-        	}, this);
+                filepaths = assets[type][name].assets;
+                filepaths.forEach(function(path){
+                        // If any patterns are "includes" (don't start with !), then include is false by default
+                        include = !pattern.some(function(step){ return step[0] !== '!'; });
+                        if( pattern && pattern.length){
+                                pattern.forEach(function(step){
+                                        if(typeof step !== 'string')
+                                                throw new Error('Include/exclude patterns passed to blockref must be string values');
+
+                                        if (step[0] === '!'){
+                                                if(minimatch(path, step.slice(1))){
+                                                        include = false;
+                                                }
+                                        } else if(minimatch(path, step)){
+                                                include = true;
+                                        }
+                                });
+                        }
+
+                        if( !include )
+                                return;
+
+                        this.push(new util.File({
+                                cwd: file.cwd,
+                                base: file.base,
+                                path: file.base + path,
+                                contents: new Buffer(fs.readFileSync( file.base + path ))
+                        }));
+                }, this);
         }else{
-        	this.emit('error', new util.PluginError('blockref', 'Could not find block type or name'));
+                this.emit('error', new util.PluginError('blockref', 'Could not find block type or name'));
         }
         cb();
-	});
+        });
 };
